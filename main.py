b9563d115fc055bb5779a5216e36b810e85d72f4
bless-otaku <blessingsbakubanya@gmail.com>
2026-03-21 14:51:21 +0100

    premier commit : exercices variable python

---
+++ main.py
@@ -0,0 +1,16 @@
+age1 = 23
+age2 = 22
+total = age1 + age2
+print(total)
+print(type(total))
+
+volume1 = 2.4
+volume2 = 3.4
+total = volume1 + volume2
+print(total)
+print(type(total))
+age1 = 23
+age2 = 22
+total = age1 + age2
+print(total)
+print(type(total))
