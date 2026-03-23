b205da96e3698ada7e87a0396153060c53c8c793
bless-otaku <blessingsbakubanya@gmail.com>
2026-03-23 22:40:39 +0100

    Mon premier commit

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-age1 = 23
-age2 = 22
-total = age1 + age2
-print(total)
-print(type(total))
+#nom = 'tom'
+#prenom = "alex"
+#age = 59
+#nom_complet = prenom + " " + nom + " " + str(age)
+#print(nom_complet)
 
-volume1 = 2.4
-volume2 = 3.4
-total = volume1 + volume2
-print(total)
-print(type(total))
-age1 = 23
-age2 = 22
-total = age1 + age2
-print(total)
-print(type(total))
+#prenom = 'bruno'
+#age = 36
+#nom = 'mars'
+#afficher = "bonjour je m appele " + prenom + " j ai " + str(age) + " ans "
+#afficher = f"bonjour je m appele {prenom} {nom} j ai {age} ans"
+#print (afficher)
+
+#prenom = input("votre nom ? ")
+#nom = input("votre prenom? ")
+#age = input("votre age? ")
+
+#print (f"bonjour {prenom} {nom} vous avez {age} ans") 
+
+
+nombre_1 = float(input("entre votre premier nombre "))
+nombre_2 = float(input("entre votre deuxieme nombre "))
+
+print("addition",nombre_1 + nombre_2)
+print("soustration",nombre_1 - nombre_2)
+print("multiplication",nombre_1 * nombre_2)
