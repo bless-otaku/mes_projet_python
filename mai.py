e8b6338002b500f4528fc249a68ac09123f88006
bless-otaku <blessingsbakubanya@gmail.com>
2026-03-30 11:09:03 +0100

    Mon second scrip python

---
+++ mai.py
@@ -0,0 +1,18 @@
+nombre_1 = float(input("entre votre nombre = "))
+nombre_2 = float(input("entre votre nombre = "))
+
+print("addition",nombre_1 + nombre_2 )
+
+
+print("soustration",nombre_1 - nombre_2 )
+
+
+print("Multiplication",nombre_1 * nombre_2 )
+
+
+print("Division",nombre_1 / nombre_2)
+
+
+
+
+
--- main.py
+++ main.py
@@ -18,9 +18,49 @@
 #print (f"bonjour {prenom} {nom} vous avez {age} ans") 
 
 
-nombre_1 = float(input("entre votre premier nombre "))
-nombre_2 = float(input("entre votre deuxieme nombre "))
+#nombre_1 = float(input("entre votre premier nombre "))
+#nombre_2 = float(input("entre votre deuxieme nombre "))
 
-print("addition",nombre_1 + nombre_2)
-print("soustration",nombre_1 - nombre_2)
-print("multiplication",nombre_1 * nombre_2)
+#print("addition",nombre_1 + nombre_2)
+#print("soustration",nombre_1 - nombre_2)
+#print("multiplication",nombre_1 * nombre_2)
+
+#username = "beni"
+#users = ["blessing","alex","beni","papa"]
+
+#nombre = int(input("entrez votre nombre : "))
+
+#if nombre % 2 ==0:
+    #print("votre nombre est pair")
+
+#else:
+    #print("votre nombre est impair")
+
+#print("Fin")
+
+
+#for i in (1,2,3,4,5,6,7,8,9):
+
+  #  print("la boucle ",i) 
+
+#i = 0
+#while i < 7 :
+
+  #   print("dans la boucle",i)
+ #    i +=1
+     
+#print("Fin")
+
+
+
+
+
+
+
+#loto = [2,8,15,22,38]
+#print(type(loto))
+#print(loto)
+
+
+
+    
