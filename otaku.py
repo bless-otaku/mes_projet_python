2b1edbebe405c2a6126f83a81980f98613c656d2
bless-otaku <blessingsbakubanya@gmail.com>
2026-04-20 13:49:43 +0100

    ajout de l exercice sur le mot de passe

---
+++ otaku.py
@@ -0,0 +1,11 @@
+Saisie = ""
+code = "Goma2026"
+
+while Saisie!=code:
+    Saisie = input("entre le mots de passe")
+    if Saisie!=code:
+        print("Mauvaise code,reessayez...")
+print("Acces autorise")
+
+
+    
